e34cdd81a5799f637d238f876db0d29e1e00d4a4
braverokmc79 <braverokmc79@gmail.com>
2022-09-21 20:51:47 +0900

    6. My SQL로 홈페이지 구현 2

--- main.js
+++ main.js
@@ -41,8 +41,18 @@ var app = http.createServer(function (request, response) {
 
       db.query("SELeCT * FROM topic", function (error, topics) {
         console.log(topics);
+
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+
+        var list = template.list(topics);
+        var html = template.HTML(title, list,
+          `<h2>${title}</h2>${description}`,
+          `<a href="/create">create</a>`
+        );
+
         response.writeHead(200);
-        response.end("success");
+        response.end(html);
       });
 
 
