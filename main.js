311389dac45153cc6a85515593b5cd3f1673a463
braverokmc79 <braverokmc79@gmail.com>
2022-09-23 00:41:14 +0900

    28. Escaping

--- main.js
+++ main.js
@@ -38,6 +38,9 @@ var app = http.createServer(function (request, response) {
   } else if (pathname === '/author/update') {
     author.update(request, response);
 
+  } else if (pathname === '/author/update_process') {
+    author.update_process(request, response);
+
   } else if (pathname === '/author/delete_process') {
     author.delete_process(request, response);
 
